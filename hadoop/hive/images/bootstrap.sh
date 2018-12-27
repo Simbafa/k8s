a2de6284da83036f6bb3aa9b5e47e48de2f90cba
root <lzw@justep.com>
2018-12-27 16:08:32 +0800

    hive with kerberos and ldap is ok

--- hadoop/hive/images/bootstrap.sh
+++ hadoop/hive/images/bootstrap.sh
@@ -8,8 +8,9 @@ fi
 . /libexec/yarn-config.sh
 . /usr/local/spark/conf/spark-env.sh
 . /usr/local/hive/conf/hive-env.sh
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
 
-kinit -kt /usr/local/hive/conf/hive.keytab hive@JUSTEP.COM
+kinit -kt /etc/hadoop/hdfs.keytab root/namenode@JUSTEP.COM
 klist 
 
 sed "s/HDFSNAMENODERPC_SERVICE_HOST/$HDFSNAMENODERPC_SERVICE_HOST/;s/HDFSNAMENODERPC_SERVICE_PORT/$HDFSNAMENODERPC_SERVICE_PORT/;s/LDAP_HOST/$LDAP_HOST/" /etc/hadoop/core-site.xml.template > /etc/hadoop/core-site.xml
@@ -17,6 +18,7 @@ cat /etc/hadoop/core-site.xml
 
 sed -i "s/JOBHISTORY_ADDRESS/$JOBHISTORY_ADDRESS/;s/JOBHISTORY_WEBAPP_ADDRESS/$JOBHISTORY_WEBAPP_ADDRESS/" /etc/hadoop/mapred-site.xml
 sed -i "s/MY_MEM_LIMIT/$MY_MEM_LIMIT/;s/MY_CPU_LIMIT/$MY_CPU_LIMIT/" /etc/hadoop/yarn-site.xml
+sed -i "s/LDAP_HOST/$LDAP_HOST/" /usr/local/hive/conf/hive-site.xml
 
 # log dir
 mkdir -p /data/hadoop-log-dir
@@ -32,8 +34,11 @@ if [ ! -f /etc/hadoop/fromhost/hive_metastore_initialized ]; then
    $HADOOP_HOME/bin/hadoop fs -mkdir -p /user/hive/warehouse
    $HADOOP_HOME/bin/hadoop fs -chmod -R 777 /tmp
    $HADOOP_HOME/bin/hadoop fs -chmod g+w /user/hive/warehouse
-   $HIVE_HOME/bin/schematool -dbType derby -initSchema --verbose &> /etc/hadoop/fromhost/hive_metastore_initialized
+   $HIVE_HOME/bin/schematool -dbType mysql -initSchema --verbose &> /etc/hadoop/fromhost/hive_metastore_initialized
 fi
+
+kinit -kt /usr/local/hive/conf/hive.keytab hive/hive@JUSTEP.COM
+klist 
 
 $HIVE_HOME/bin/hive --service metastore &
 $HIVE_HOME/bin/hive --service hiveserver2
